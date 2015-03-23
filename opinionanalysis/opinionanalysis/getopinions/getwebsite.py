ef225ce5c37776643c9ca9e43b82bd584d1cb99c
vijayanajay <vijayan.ajay@gmail.com>
2015-03-23 20:55:04 +0530

    Get single url from feed

--- opinionanalysis/opinionanalysis/getopinions/getwebsite.py
+++ opinionanalysis/opinionanalysis/getopinions/getwebsite.py
@@ -1,24 +1,19 @@
-from bs4 import BeautifulSoup
-from urllib2 import urlopen
-import re
 import logging
+import feedparser
 from goose import Goose
 logger = logging.getLogger(__name__)
 
+
 def extract(url):
-	logger.info ("inside extract function")
-	goose = Goose()
-	article = goose.extract(url=url)
-	logger.debug (article.cleaned_text)
-	return (article)
+    logger.info("inside extract function")
+    goose = Goose()
+    article = goose.extract(url=url)
+    logger.debug(article.cleaned_text)
+    return (article)
 
-def clean_post(post):
-	return ("nothing")
 
-if __name__ == '__main__':
-	url = "http://www.team-bhp.com/forum/test-drives-initial-ownership-reports/148583-2014-fiat-linea-facelift-test-drive-review.html"
-
-	soup = make_soup(url)
-	f = open('workfile.tmp', 'a')
-
- 	post = get_soup(url)
+def get_feed(feed):
+    logger.info("inside get_feed")
+    feed_content = feedparser.parse(feed)
+    links_in_feed = feed_content['entries'][1]['links']
+    return links_in_feed[0]['href']
--- opinionanalysis/opinionanalysis/getopinions/views.py
+++ opinionanalysis/opinionanalysis/getopinions/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render 
+from django.shortcuts import render
 import getwebsite
 import logging
-from goose import Goose
 logger = logging.getLogger(__name__)
 
-def index (request):
-	url = "http://www.topgear.com/india/our-car-reviews/linea?id=2833"
-	#post_text = get the results from soup based on the url above
-	article= getwebsite.extract(url)
-	
-	# the return from get_soup is a ResultSet (list). That needs to be converted to dictionary to send to the html
-	context = {'post_text': article.cleaned_text}
 
-	#return the dictionary to the html
-	return render(request,'getopinions/index.html', context)
+def index(request):
+    url = "http://www.topgear.com/india/our-car-reviews/linea?id=2833"
+    rssfeed = "http://www.livemint.com/rss/opinion"
+
+# get feed contents from rssfeed
+    feed_content = getwebsite.get_feed(rssfeed)
+
+# post_text = get the results from soup based on the url above
+    article = getwebsite.extract(url)
+
+# the return from get_soup is a ResultSet (list).
+# That needs to be converted to dictionary to send to the html
+    context = {'post_text': feed_content}
+
+#    context = {'post_text': article.cleaned_text}
+
+# return the dictionary to the html
+    return render(request, 'getopinions/index.html', context)
