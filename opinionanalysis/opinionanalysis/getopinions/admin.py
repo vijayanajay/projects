cb9d9d3b87f618096875ab3191a472ce5f0fc77a
vijayanajay <vijayan.ajay@gmail.com>
2015-03-28 20:19:19 +0530

    Added new Article table. Added in admin screen

--- opinionanalysis/opinionanalysis/getopinions/admin.py
+++ opinionanalysis/opinionanalysis/getopinions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from getopinions.models import *
+from django.contrib import admin
+
+admin.site.register(Article)
--- opinionanalysis/opinionanalysis/getopinions/models.py
+++ opinionanalysis/opinionanalysis/getopinions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+
+	site_name = models.CharField(max_length=30)
+	title = models.CharField(max_length=256)
+	content = models.CharField(max_length=1024)
+	link = models.CharField(max_length=1024)
+
