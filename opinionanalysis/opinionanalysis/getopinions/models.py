9ce6d67a75138c650c50eea6eb271abef30b513e
vijayanajay <vijayan.ajay@gmail.com>
2015-03-29 17:39:40 +0530

    Inserted some more columms to Article. Made Article link unique

--- opinionanalysis/opinionanalysis/getopinions/models.py
+++ opinionanalysis/opinionanalysis/getopinions/models.py
@@ -5,6 +5,7 @@ class Article(models.Model):
 
 	site_name = models.CharField(max_length=30)
 	title = models.CharField(max_length=256)
-	content = models.CharField(max_length=1024)
-	link = models.CharField(max_length=1024)
+	summary = models.CharField(max_length=1024, null=True)
+	content = models.CharField(max_length=1024, null=True)
+	link = models.CharField(max_length=1024, unique=True)
 
