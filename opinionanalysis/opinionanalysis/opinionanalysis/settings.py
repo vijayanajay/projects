8861469d2296d92a856ed8985297ae2ace624acc
vijayanajay <vijayan.ajay@gmail.com>
2015-04-03 18:03:25 +0530

    fix #1: Installed the following utilities to ensure the test environment is setup behave selenium parse django-behave mechanize wsgi_intercept

--- opinionanalysis/opinionanalysis/opinionanalysis/settings.py
+++ opinionanalysis/opinionanalysis/opinionanalysis/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'getopinions'
+    'getopinions',
+    'debug_toolbar',
 )
 
 MIDDLEWARE_CLASSES = (
