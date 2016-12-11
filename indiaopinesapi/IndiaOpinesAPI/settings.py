d6d0c1478d7400c3103155f4a4d6a350ac79ed12
vijayanajay <vijayan.ajay@gmail.com>
2016-12-11 17:38:22 +0530

    testing svn setup in eclipse

--- indiaopinesapi/IndiaOpinesAPI/settings.py
+++ indiaopinesapi/IndiaOpinesAPI/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import grabber
+from grabber.apps import GrabberConfig
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'grabber'
 ]
 
 MIDDLEWARE = [
