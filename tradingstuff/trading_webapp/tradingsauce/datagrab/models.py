49d4f3068e9b90d2ef761752d8295458ce683f15
vijayanajay <vijayan.ajay@gmail.com>
2017-07-17 21:57:50 +0530

    added StockHistory and StockSymbol tables and migrated

--- tradingstuff/trading_webapp/tradingsauce/datagrab/models.py
+++ tradingstuff/trading_webapp/tradingsauce/datagrab/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class StockSymbol(models.Model):
+    name =  models.CharField (max_length = 50)
+    symbol = models.CharField (max_length = 10)
+    tickerNumber = models.IntegerField()
+    
+class StockHistory(models.Model):
+    symbol = models.ForeignKey (StockSymbol, 
+                                on_delete = models.CASCADE,
+                                )
+    date = models.DateTimeField()
+    openPrice = models.FloatField()
+    highPrice = models.FloatField()
+    lowPrice = models.FloatField()
+    closePrice = models.FloatField()
+    wap = models.FloatField()
+    numberOfShares = models.IntegerField()
+    numberOfTrades = models.IntegerField()
+    totalTurnover = models.FloatField()
+    spreadHighLow = models.FloatField()
+    spreadCloseOpen = models.FloatField()
+    
