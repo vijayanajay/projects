5dca3fb6b103f4b448309be1a6bd2791765a8ad2
vijayanajay <vijayan.ajay@gmail.com>
2017-07-23 20:23:55 +0530

    migrated adding a filestorage system attr for StockSymbol

--- tradingstuff/trading_webapp/tradingsauce/datagrab/models.py
+++ tradingstuff/trading_webapp/tradingsauce/datagrab/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
+fs = FileSystemStorage(location = settings.BASE_DIR + '/media/csvData')
 
 # Create your models here.
 class StockSymbol(models.Model):
     name =  models.CharField (max_length = 50)
     symbol = models.CharField (max_length = 10)
     tickerNumber = models.IntegerField()
+    file = models.FileField(storage = fs, default = fs)
     
     def __str__(self):
        return self.name
