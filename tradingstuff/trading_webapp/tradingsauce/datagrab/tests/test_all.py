2b22e2544f263d95d6fb768d13686daded6cd263
vijayanajay <vijayan.ajay@gmail.com>
2017-08-19 19:11:35 +0530

    created a utils file

--- tradingstuff/trading_webapp/tradingsauce/datagrab/tests/test_all.py
+++ tradingstuff/trading_webapp/tradingsauce/datagrab/tests/test_all.py
@@ -4,6 +4,9 @@ import random
 import string
 import factory
 from datagrab.models import StockSymbol, StockHistory
+from django.test import Client
+from django.core.urlresolvers import reverse
+from django.conf import settings
 
 def random_text(length=10):
     return u''.join(random.choice(string.ascii_letters) for x in range(length))
@@ -31,7 +34,12 @@ class DataGrab(TestCase):
         stock = StockSymbol.objects.get(tickerNumber = '500477')
         assert str(stock), 'ASHOK Leyland'
         
-    
+    def test_derived_csv_filename_is_correct(self):
+        stock = StockSymbolFactory.create(name = 'ASHOK Leyland', 
+                                           symbol = 'ASHOKLEY',
+                                           tickerNumber = '500477')
+        filename = settings.BASE_DIR + '\csvData\\' + str(
+                stock.tickerNumber) + '.csv'        
         
         
 # Create your tests here.
