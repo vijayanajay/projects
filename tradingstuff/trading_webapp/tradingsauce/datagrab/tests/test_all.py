e169699a011be43356a1b40d75f64d1edeba7fac
vijayanajay <vijayan.ajay@gmail.com>
2017-08-19 15:53:17 +0530

    random commit

--- tradingstuff/trading_webapp/tradingsauce/datagrab/tests/test_all.py
+++ tradingstuff/trading_webapp/tradingsauce/datagrab/tests/test_all.py
@@ -1,15 +1,31 @@
 from django.test import TestCase
-import pytest
+#import pytest
+import random
+import string
+import factory
 from datagrab.models import StockSymbol, StockHistory
 
+def random_text(length=10):
+    return u''.join(random.choice(string.ascii_letters) for x in range(length))
+
+def random_number(length=6):
+    return u''.join(random.choice(string.digits) for x in range(length))
+
+
+class StcokSymbolFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = StockSymbol
+        abstract = False
+    
+    name = factory.LazyAttribute(lambda t: random_text(length = 50))
+    symbol = factory.LazyAttribute(lambda t: random_text(length = 10))
+    tickerNumber = factory.LazyAttribute(lambda t: random_number(length = 6))
+    
+    
 class DataGrab(TestCase):
-    def setUp(self):
-        someObject = StockSymbol.objects.create(name = 'ASHOK Leyland', 
-                                           symbol = 'ASHOKLEY',
-                                           tickerNumber = '500478')
     
     def test_stocksymbol_stringrepresentation(self):
-        stock = StockSymbol.objects.create(name = 'ASHOK Leyland', 
+        stock = StcokSymbolFactory.create(name = 'ASHOK Leyland', 
                                            symbol = 'ASHOKLEY',
                                            tickerNumber = '500477')
         stock = StockSymbol.objects.get(tickerNumber = '500477')
