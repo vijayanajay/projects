6aaf2f499738db7b4c257ba4dcbe71a258c61c94
vijayanajay <vijayan.ajay@gmail.com>
2017-11-12 22:05:53 +0530

    tried to fix shit to insert sma3 which still does not work

--- tradingstuff/trading_webapp/tradingsauce2/datagrab/utils.py
+++ tradingstuff/trading_webapp/tradingsauce2/datagrab/utils.py
@@ -16,11 +16,13 @@ def get_csv_filename(stocksymbol):
     return file_name
 
 def read_csv_file(stock, file_name):
+    log.debug("inside read_csv_file")
     try:
         dataReader = csv.reader(open(file_name), delimiter=',')
     except:
         #assuming it's always filenotfounderror. Can be something else too,
         #to check later
+        log.debug("completed read_csv_file with exception in reading csv file")
         return False
     try:
         for line in dataReader:
@@ -45,19 +47,25 @@ def read_csv_file(stock, file_name):
             history.spreadCloseOpen = line[12]
             history.save()
     except:
+        log.debug("completed read_csv_file with exception in inserting into db")
         return 1
+    log.debug("completed read_csv_file")
     return dataReader
 
 def calculate_and_store_sma3(stock):
-    stockHistoryDF = pd.DataFrame(list(StockHistory.objects.filter(symbol = stock).order_by('date').values('date', 'closePrice')))
-    stockHistoryDF.insert(2, column="SMA", value=pd.Series(stockHistoryDF["closePrice"]).rolling(window=3).mean())
+    log.debug("inside utils.calculate_and_store_sma3")
+    stockHistoryDF = pd.DataFrame(list(StockHistory.objects.filter(symbol = stock).order_by('date').values('id', 'date', 'closePrice')))
+    originalDF = pd.DataFrame(list(StockHistory.objects.filter(symbol = stock).order_by('date').values('id', 'date', 'closePrice','sma3')))
+    stockHistoryDF.insert(3, column="SMA", value=pd.Series(stockHistoryDF["closePrice"]).rolling(window=3).mean())
     stockHistoryDF = stockHistoryDF.fillna(method='bfill')
     temp_list = []
+
     for n in range(0, len(stockHistoryDF), 1):
-        row = StockHistory.objects.filter(symbol = stock, date = stockHistoryDF.iloc[n,1])
-        temp_list.insert(n, n)
-        if row[0].sma3 == stockHistoryDF.iloc[n,2] and row[0].sma3 != None:
+        row = StockHistory.objects.filter(id = stockHistoryDF.iloc[n,2])
+        #row_real = originalDF.loc(originalDF['id'] == stockHistoryDF.iloc[n,2])
+        #
+        if row[0].sma3 == stockHistoryDF.iloc[n,3] and row[0].sma3 != None:
             continue
-        row[0].sma3 = stockHistoryDF.iloc[n,2]
+        row[0].sma3 = stockHistoryDF.iloc[n,3]
         row[0].save()
-    return temp_list
+    return "true"
