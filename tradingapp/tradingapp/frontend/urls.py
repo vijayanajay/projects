7a9f26c835580ce00cda5f4a35da59f700064b6a
vijayanajay <vijayan.ajay@gmail.com>
2020-04-27 19:46:11 +0200

    got css and templates working

--- tradingapp/tradingapp/frontend/urls.py
+++ tradingapp/tradingapp/frontend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-        path('test', test, name = 'test')
+        path('test', test, name = 'test'),
+        path('refresh_data', refresh_data, name = 'refresh_data')
 ]
--- tradingapp/tradingapp/frontend/views.py
+++ tradingapp/tradingapp/frontend/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.conf import settings
+import os
 from django.http import HttpResponse
 from .models import Price, Company
 import pandas as pd
@@ -8,8 +10,10 @@ import csv, datetime, quandl
 quandl.ApiConfig.api_key = 'fRsTyQJZaBbXBcKsnahq'
 
 def test(request):
-    stock_price = quandl.get('BSE/BOM500325')
-    return HttpResponse(len(stock_price))
+    #stock_price = quandl.get('BSE/BOM500325')
+    #text = len(stock_price)
+    text = os.path.join(settings.BASE_DIR, 'templates\\')
+    return HttpResponse(text)
 
 def refresh_data(request):
     test_data = "Ajay"
