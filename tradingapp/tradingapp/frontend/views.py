e372834c6ba22c2b988bff9c4473423bba65ca38
vijayanajay <vijayan.ajay@gmail.com>
2020-04-27 21:25:23 +0200

    cleaned up unnecessary stuff add site.css to have some custom changes

--- tradingapp/tradingapp/frontend/views.py
+++ tradingapp/tradingapp/frontend/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.conf import settings
-import os
 from django.http import HttpResponse
 from .models import Price, Company
 import pandas as pd
@@ -12,7 +11,6 @@ quandl.ApiConfig.api_key = 'fRsTyQJZaBbXBcKsnahq'
 def test(request):
     #stock_price = quandl.get('BSE/BOM500325')
     #text = len(stock_price)
-    text = os.path.join(settings.BASE_DIR, 'templates\\')
     return HttpResponse(text)
 
 def refresh_data(request):
