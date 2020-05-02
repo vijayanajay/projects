5f11c1638ca911445798c3ec4e6f51e315a1a9d3
vijayanajay <vijayan.ajay@gmail.com>
2020-05-02 22:03:04 +0200

    created automatic table for refresh_data

--- tradingapp/tradingapp/frontend/urls.py
+++ tradingapp/tradingapp/frontend/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
         path('test', test),
-        path('', data_index)
+        path('refresh_data/<int:companyid>', refresh_data, name='refresh_data'),
+        path('refresh_data/', refresh_data, name='refresh_data'),
+        path('', data_index),
 ]
