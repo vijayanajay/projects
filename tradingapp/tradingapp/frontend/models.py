59d400c90959b4e4e29c9a6d265523d42b94ef49
vijayanajay <vijayan.ajay@gmail.com>
2020-07-14 23:16:04 +0200

    Added Company.yahoo_id

--- tradingapp/tradingapp/frontend/models.py
+++ tradingapp/tradingapp/frontend/models.py
@@ -7,6 +7,8 @@ class Company(models.Model):
     name = models.CharField(max_length=200)
     bom_id = models.CharField(max_length = 20, 
                               unique=True, null=False)
+    yahoo_id = models.CharField(max_length=20, unique=True,
+                                null=True, blank=True)
     last_updated_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
