0fc9811a80e4476b660070c92a8d9d3ba7d9f749
vijayanajay <vijayan.ajay@gmail.com>
2020-11-29 19:16:23 +0100

    reduced data retrieval to 3 months

--- tradingapp/tradingapp/frontend/views.py
+++ tradingapp/tradingapp/frontend/views.py
@@ -28,6 +28,7 @@ def test(request):
 
 
 def refresh_data(request, id):
+    logger.debug ("Inside frontend.refresh_data")
     company = Company.objects.get(id=id)
     if company.last_updated_date is None:
         stock_history = yf(company.yahoo_id, result_range='7d', interval='1m').result
@@ -38,9 +39,13 @@ def refresh_data(request, id):
         stock_history = pd.DataFrame()
         stock_history_daily = pd.DataFrame()
         latest_price_history = IntradayPrice.objects.latest('created_at').created_at.astimezone(tz=tz)
-        latest_daily_price_history = DailyPrice.objects.latest('created_at').created_at.astimezone(tz=tz)
+        latest_daily_price_history = DailyPrice.objects.latest('created_at').date
         stock_history = yf(company.yahoo_id, result_range='7d', interval='1m').result
-        stock_history_daily = yf(company.yahoo_id, result_range='5y', interval='1d').result
+        logger.debug("latest daily price history = " + str(latest_daily_price_history))
+        latest_daily_price_history = '2020-11-13'
+        stock_history_daily = yf(company.yahoo_id, result_range='3mo', interval='1d').result
+        logger.debug ("stock daily history df (below) = ")
+        logger.debug (stock_history_daily)
         stock_history['date'] = stock_history.index.tz_localize(tz=tz)
         stock_history_daily['date'] = stock_history_daily.index.tz_localize(tz=tz)
         stock_history.date = pd.to_datetime(stock_history.date, unit='s')
@@ -52,7 +57,9 @@ def refresh_data(request, id):
                                                   (stock_history_daily.date <= max_daily_date)]
         utils.insert_into_db(stock_history, company.id, 'yahoo', '1m')
         debuginfo = utils.insert_into_db(stock_history_daily, company.id, 'yahoo', '1d')
-    request.session['debuginfo'] = debuginfo
+
+    #request.session['debuginfo'] = debuginfo
+    logger.debug ("Completed frontend.refresh_data")
     return redirect('data_index')
 
 
