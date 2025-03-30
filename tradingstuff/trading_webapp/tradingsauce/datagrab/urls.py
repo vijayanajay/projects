570b0e783f8738088f1c5b9152c2ae4e8499f24b
vijayanajay <vijayan.ajay@gmail.com>
2025-03-30 22:43:14 +0530

    fixed issues

--- tradingstuff/trading_webapp/tradingsauce/datagrab/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from datagrab import views
-
-urlpatterns = [
-        url(r'^$', views.index, name='index'),
-]
--- tradingstuff/trading_webapp/tradingsauce2/datagrab/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatagrabConfig(AppConfig):
-    name = 'datagrab'
